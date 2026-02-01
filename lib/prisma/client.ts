40e0a49c98a01f9e9dbc70679373a6aae4788ce8
chandrabosep <chandrabosep3112@gmail.com>
2026-02-01 12:56:04 +0530

    feat: add Prisma dependency and enhance navbar with dynamic links

---
+++ lib/prisma/client.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalForPrisma = global as unknown as {
+	prisma?: PrismaClient;
+	pool?: Pool;
+};
+
+// Create a connection pool for the pooled DATABASE_URL
+const pool =
+	globalForPrisma.pool ||
+	new Pool({ connectionString: process.env.DATABASE_URL });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.pool = pool;
+
+// Create the adapter
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		adapter,
+		log: ["query", "info", "warn", "error"],
+	});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
